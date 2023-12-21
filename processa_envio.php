197152d98d2728a7be194dd69582048d4387a2ef
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-21 10:25:58 -0300

    criando e instanciando a classe Mensagem na pagina processa_envio.php

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,51 @@
 <?php 
     
-    print_r($_POST);
+    //print_r($_POST);
+    
+    class Mensagem {
+
+      private $para = null;
+      private $assunto = null;
+      private $mensagem = null;
+
+      public function __get($atributo) {
+        return $this->$atributo;
+      }
+
+      public function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+      }
+
+      public function validaMensagem() {
+        //validar os dados (verificar se formulario foi preenchido certo)
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) { 
+          return false;
+        }
+        //se todos estiverem preeechidas recebe true
+        return true;
+
+      }
+
+    }
+
+    //criar um variavel que vai receber uma instancia da class msg
+    $mensagem = new Mensagem(); 
+
+    //recuperar os dados que vem do metodo post(super global POST)
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+    echo '<br>';
+    //print_r($mensagem);
+
+    if ($mensagem->validaMensagem()) {
+      echo 'Mensagem é válida';  //tudo foi preenchido corretamente no formulario
+    } else {
+      echo 'Mensagem invalida';   //formulario não foi preenchido corretamente
+    }
+
+
+
+
 
 ?>
