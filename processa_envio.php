9ca8de6dea1a5d23c7be4fa8293b583583053952
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-22 11:49:06 -0300

    Configurando o PHPMailer e enviando e-mail

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,19 @@
 <?php 
     
+    //importacao dos arquivos(require)
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+
+
+
+
     //print_r($_POST);
     
     class Mensagem {
@@ -16,7 +30,7 @@
         $this->$atributo = $valor;
       }
 
-      public function validaMensagem() {
+      public function mensagemValida() {
         //validar os dados (verificar se formulario foi preenchido certo)
         if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) { 
           return false;
@@ -36,13 +50,50 @@
     $mensagem->__set('assunto', $_POST['assunto']);
     $mensagem->__set('mensagem', $_POST['mensagem']);
     echo '<br>';
-    //print_r($mensagem);
 
-    if ($mensagem->validaMensagem()) {
-      echo 'Mensagem é válida';  //tudo foi preenchido corretamente no formulario
-    } else {
-      echo 'Mensagem invalida';   //formulario não foi preenchido corretamente
-    }
+    print_r($mensagem);
+
+    if (!$mensagem->mensagemValida()) {
+      echo 'Mensagem não é válida';  //caso a msg não seja valida retorna false e entra no if
+      die();  //mata processamento do script
+    } 
+
+    $mail = new PHPMailer(true);
+    try {
+      //Server settings
+      $mail->SMTPDebug = 2;                      //Enable verbose debug output
+      $mail->isSMTP();                                            //Send using SMTP
+      $mail->Host       = 'smtp.gmail.com.';                     //Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+      $mail->Username   = 'jm8661990@gmail.com';                     //SMTP username
+      $mail->Password   = 'ueih gbop yhmy mtca';                               //SMTP password
+      $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+      $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+  
+      //Recipients
+      $mail->setFrom('jm8661990@gmail.com', 'web completo remetente');
+      $mail->addAddress($mensagem->__get('para'));     //Add a recipient
+             //Name is optional
+      //$mail->addReplyTo('info@example.com', 'Information');
+      //$mail->addCC('cc@example.com');
+      //$mail->addBCC('bcc@example.com');
+  
+      //Attachments
+      //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+      //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+  
+      //Content
+      $mail->isHTML(true);                                  //Set email format to HTML
+      $mail->Subject = $mensagem->__get('assunto');
+      $mail->Body    = $mensagem->__get('mensagem');
+      $mail->AltBody = 'É necessário usar um client que suporte HTML para ter acesso total ao conteudo dessa mensagem';
+  
+      $mail->send();
+      echo 'E-mail enviado com sucesso';
+  } catch (Exception $e) {
+      echo "Não foi possivel enviar esse e-mail!.Por favor tente novamente mais tarde!";
+      echo " Detalhes do erro:" . $mail->ErrorInfo;
+  }
 
 
 
